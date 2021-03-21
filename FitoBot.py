0ad3239a445fea0c8fb41204cf810999286e49e1
Danil Malkov <77438920+DMalkov61@users.noreply.github.com>
2021-03-22 00:36:10 +0200

    Update FitoBot.py

--- FitoBot.py
+++ FitoBot.py
@@ -14,6 +14,16 @@ def handle_start(message):
     user_markup.row('Кошкодевочка', 'Мемчик')
     user_markup.row('Музяо в лабу', 'Мотивирующая цитата')
     bot.send_message(message.from_user.id, 'Привет,Босс, Чем могу помочь? /start', reply_markup=user_markup)
+@bot.message_handler(content_types=['text']) #Вставим музычку
+def handle_text (message):
+    if message.text == 'Музяо в лабу':
+        directory = 'D:/Telega/Music'
+        all_files_in_directory = os.listdir(directory)
+        random_file = random.choice(all_files_in_directory)
+        aud = open(directory + '/' +random_file, 'rb')
+        bot.send_chat_action(message.from_user.id, 'upload_audio')
+        bot.send_audio(message.from_user.id, aud)
+        aud.close()
 @bot.message_handler(commands=['help'])
 def help_message(message):
     bot.send_message(message.from_user.id,
