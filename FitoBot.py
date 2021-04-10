8f49916168d5ee613f86910786972dcb87e470c9
Danil Malkov <77438920+DMalkov61@users.noreply.github.com>
2021-04-10 08:54:49 +0200

    Update FitoBot.py

--- FitoBot.py
+++ FitoBot.py
@@ -16,7 +16,7 @@ def handle_start(message):
     user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
     user_markup.row('/start', "/stop")
     user_markup.row('Кошкодевочка', 'Мемчик')
-    user_markup.row('Музяо в лабу', 'Мотивирующая цитата')
+    user_markup.row('Музяо в лабу', 'Таймер')
     bot.send_message(message.from_user.id, 'Привет,Босс, Чем могу помочь? /start', reply_markup=user_markup)
 
 @bot.message_handler(commands=['help'])
