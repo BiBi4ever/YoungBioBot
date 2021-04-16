ae066214a617dcd96245931b0711139a4894ceb8
Danil Malkov <77438920+DMalkov61@users.noreply.github.com>
2021-04-16 23:17:33 +0200

    Update FitoBot.py

--- FitoBot.py
+++ FitoBot.py
@@ -19,7 +19,7 @@ def handle_start(message):
     user_markup.row('Музыка', 'Таймер')
     bot.send_message(message.from_user.id, 'Привет,Босс, Чем могу помочь? /start', reply_markup=user_markup)
     if message.text == 'Таймер':
-    bot.register_next_step_handler(message, timer_menu)
+        bot.register_next_step_handler(message, timer_menu)
     
 def features(message):
     return_row = types.ReplyKeyboardMarkup(True, True)
