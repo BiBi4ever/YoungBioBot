1a85c5eadaf9bb5979dbfb4fbf7d228a14080c75
Danil Malkov <77438920+DMalkov61@users.noreply.github.com>
2021-04-18 23:40:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8-*-
-TOKEN = 1460887912:AAF35lGmnMPoo8UpHEXKMBeq-4DwCZ4nKBI
+import os
+TOKEN = os.environ.get('TOKEN')
 client_secret_calendar='/BiBi4ever/YoungBioBot/client_secret.json'
